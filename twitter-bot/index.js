830b223d6261d235d59c5cd2a14ed3b1f55ee39e
Bruno Peternella <brunopeternella@outlook.com>
2021-11-26 08:54:57 -0300

    alterado para consumir uma API de palavras BR

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -3,7 +3,8 @@ const crypto = require('crypto');
 const OAuth = require('oauth-1.0a');
 const got = require('got');
 const endpointURL = `https://api.twitter.com/2/tweets`;
-const wordGeneratorURL = 'https://random-word-api.herokuapp.com/word';
+//const wordGeneratorURL = 'https://random-word-api.herokuapp.com/word';
+const wordGeneratorURL = 'https://api.dicionario-aberto.net/random';
 
 const consumerKeys = {
     key: process.env.CONSUMER_KEY,
@@ -28,15 +29,15 @@ const oauth = OAuth({
 
 function generateWord(){
     got.get(wordGeneratorURL).then(res => {
-        let _word = JSON.parse(res.body);
-        return postRequest(_word[0]);
+        let body = JSON.parse(res.body);
+        return postRequest(body.word);
        })
 }
 
 async function postRequest(word) {
 
     const data = {
-        "text": `The word of the day is: ${word}`
+        "text": `${word}`
     };
 
     console.log(data);
